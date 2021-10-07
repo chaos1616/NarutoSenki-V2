20d2007383529c3895c4269562bbe364adfb9c37
Re <reaperofblack@gmail.com>
2021-10-07 16:54:59 +0800

    add dos2unix tool

--- tools/tolua++/build.bat
+++ tools/tolua++/build.bat
@@ -1,1 +1,4 @@
-..\tolua++.exe -L basic.lua -o "../../scripting/lua/cocos2dx_support/LuaCocos2d.cpp" Cocos2d.pkg
+
+..\tolua++.exe -L basic.lua -o ../../scripting/lua/cocos2dx_support/LuaCocos2d.cpp Cocos2d.pkg
+
+..\dos2unix.exe ../../scripting/lua/cocos2dx_support/LuaCocos2d.cpp
